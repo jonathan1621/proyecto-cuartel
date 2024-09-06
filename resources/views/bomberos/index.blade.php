5e6a963ad978e550bcd0865c3bcc3d21e87adabb
jonathan1621 <jonathan16_21@hotmail.com>
2024-09-06 17:29:19 -0300

    Primer commit

---
+++ resources/views/bomberos/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout/template')
+
+@section('title', 'Bomberos | Cuartel')
+
+@section('contenido')
+
+<main>
+    <div class="container py-4">
+        <h2>Listado de bomberos</h2>
+
+        <a href=""></a>
+
+    </div>
+</main>
